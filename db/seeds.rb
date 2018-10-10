1434c984814ec61525bfd073b806785681f306bb
Paul Campbell <nimbleltd@gmail.com>
2018-10-10 06:29:15 -0500

    Implemented seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!([
+  {
+    name: "BugSmash",
+    location: "Denver",
+    price: 10.00,
+    description: "A night of smashing the bugs",
+    starts_at: 10.days.from_now
+  },
+  {
+    name: "Hackathon",
+    location: "Austin",
+    price: 40.00,
+    description: "Hunker down for a Hackathon Hoedown",
+    starts_at: 21.days.from_now
+  },
+  {
+    name: "Kata Camp",
+    location: "Dallas",
+    price: 75.00,
+    description: "Grow your craft with katas",
+    starts_at: 30.days.from_now
+  }
+])
