6e568c0ce0d0c7f167e35b6fa8fb40365aef83b6
Paul Campbell <nimbleltd@gmail.com>
2018-10-16 14:28:00 -0500

    Implemented creating a new event, present reuslts and refactor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,15 +13,30 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    event_params = params.require(:event).permit(
+    @event.update(event_params)
+    redirect_to @event
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
+private
+
+  def event_params
+    params.require(:event).permit(
                     :name,
                     :description,
                     :location,
                     :price,
                     :starts_at
                       )
-    @event.update(event_params)
-    redirect_to @event
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update" 
+  # get "events" => "events#index"
+  # get "events/new" => "events#new", as: "new_event"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update" 
+
+  resources :events
+
 end
